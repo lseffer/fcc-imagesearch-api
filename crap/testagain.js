a8020d67d5d4b9da1cb551f7b5d1e01cad499747
Leonard Seffer <leonardseffer@gmail.com>
2017-03-23 18:25:20 +0000

    Remove the now ignored directory "crap"

--- crap/testagain.js
+++
@@ -1,9 +0,0 @@
-var testapi = require("./bing_search.js");
-testapi.searchBing('mouse',0, function(data){
-    console.log(JSON.stringify(data, null, 4));
-});
-
-// testapi.searchBing('mouse',0);
-
-    
-// console.log(JSON.stringify(results, null, 4));
